b667de2e4261783168d421b7a9d546ec5a9946a5
Lars Busk <lab@foss.dk>
2021-10-07 12:31:21 +0200

    Add FT3 cuvette wear tool

---
+++ FT3CuvetteWear/IrmaCuvetteWear/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IrmaCuvetteWear
+{
+  public partial class Form1 : Form
+  {
+    
+    private string folderName;
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void browseButton_Click(object sender, EventArgs e)
+    {
+        FolderBrowserDialog dialog = new FolderBrowserDialog();
+
+        if (!dialog.ShowDialog().Equals(DialogResult.Cancel))
+        {
+          string folder = dialog.SelectedPath;// "C:\\Users\\lab\\Projects\\GitHub\\TestAndServiceTools\\FT3CuvetteWear\\NovaLogs";
+          CreateCsvFile(folder);
+        }
+    }
+
+    private void CreateCsvFile(string folder)
+    {
+      //string folder = dialog.SelectedPath;// "C:\\Users\\lab\\Projects\\GitHub\\TestAndServiceTools\\FT3CuvetteWear\\NovaLogs";
+      var hostFiles = ServerHostfiles(folder);
+      var info = CuvetteLines(hostFiles);
+      SaveToCsv(info);
+    }
+
+    private List<string> ServerHostfiles(string folder)
+    {
+      var files = Directory.GetFiles(folder).Where(f => f.Contains("IrmaDairyServerHost"));
+      return files.ToList();
+    }
+
+    private List<WearInfo> CuvetteLines(List<string> files)
+    {
+      List<WearInfo> wearInfos= new List<WearInfo>();
+      foreach (var logFile in files)
+      {
+        var lines = File.ReadAllLines(logFile).Where(l => l.Contains("Updating the SBRef average."));
+        foreach (var line in lines)
+        {
+          wearInfos.Add(GetWearInfo(line));
+        }
+      }
+
+      wearInfos.Sort();
+      return wearInfos;
+    }
+
+    private WearInfo GetWearInfo(string logLine)
+    {
+      string[] lineParts = logLine.Split(' ');
+      var info = new WearInfo();
+      info.Date = lineParts[0];
+      info.Time = lineParts[1];
+      string correctionPart = lineParts.First(lp => lp.StartsWith("intensityCorrection"));
+      info.Correction = correctionPart.Split('=')[1];
+
+      return info;
+    }
+
+    private void SaveToCsv(List<WearInfo> infos)
+    {
+      List<string> csvLines = new List<string>();
+      csvLines.Add("Date;Time;Correction");
+      foreach (var wearInfo in infos)
+      {
+        csvLines.Add(wearInfo.ToString());
+      }
+
+      File.WriteAllLines("WearLog.csv", csvLines);
+    }
+
+    private void Form1_DragDrop(object sender, DragEventArgs e)
+    {
+      var s = e.Data.GetData(DataFormats.FileDrop);
+      string name = ((string[])s)[0];
+      CreateCsvFile(name);
+    }
+
+    private void Form1_DragEnter(object sender, DragEventArgs e)
+    {
+      if (e.Data.GetDataPresent(DataFormats.FileDrop))
+        e.Effect = DragDropEffects.Copy;
+      else
+        e.Effect = DragDropEffects.None;
+    }
+
+    private void dropTextBox_DragEnter(object sender, DragEventArgs e)
+    {
+      if (e.Data.GetDataPresent(DataFormats.FileDrop))
+        e.Effect = DragDropEffects.Copy;
+      else
+        e.Effect = DragDropEffects.None;
+    }
+
+    private void dropTextBox_DragDrop(object sender, DragEventArgs e)
+    {
+      var s = e.Data.GetData(DataFormats.FileDrop);
+      string name = ((string[]) s)[0];
+      CreateCsvFile(name);
+    }
+  }
+}
---
+++ FT3CuvetteWear/IrmaCuvetteWear/WearInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IrmaCuvetteWear
+{
+  public class WearInfo: IComparable
+  {
+    public string Date;
+    public string Time;
+    public string Correction;
+
+    public int CompareTo(object obj)
+    {
+      return this.weardateTime().CompareTo(((WearInfo)obj).weardateTime());
+    }
+
+    public override string ToString()
+    {
+      return $"{Date};{Time};{Correction.Replace('.', ',')}";
+    }
+
+    private DateTime weardateTime()
+    {
+      string dtString = $"{Date}T{Time.Substring(0, 8)}Z";
+      return DateTime.Parse(dtString, new DateTimeFormatInfo());
+    }
+  }
+}
