384d50be5fc69cd0cebd791e9fc0bc5a2563b252
Lars Busk <lab@foss.dk>
2021-10-08 09:34:14 +0200

    Added small progrma to help test file export from Nova

---
+++ TestExport/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace TestExport
+{
+  class Program
+  {
+    static string fileName = "DeletedFiles.csv";
+    static void Main(string[] args)
+    {
+      FileSystemWatcher watcher = new FileSystemWatcher("C:\\Export");
+
+      watcher.EnableRaisingEvents = true;
+      watcher.Created += Watcher_Created;
+
+      File.AppendAllText(fileName, "DeletedTime;Filename\n");
+
+      Console.Read();
+    }
+
+    private static void Watcher_Created(object sender, FileSystemEventArgs e)
+    {
+      Thread.Sleep(1000);
+      string line = $"{DateTime.Now};{e.Name}\n";
+      File.Delete(e.FullPath);
+      File.AppendAllText(fileName, line);
+    }
+  }
+}
