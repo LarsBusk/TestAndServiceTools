6c396ba7f54cad1c4bf1549738d4e56e4f59a022
Lars Busk <lab@foss.dk>
2023-06-21 15:16:31 +0200

    Small tool to test export from Nova

--- TestExport/Program.cs
+++ TestExport/Program.cs
@@ -10,16 +10,20 @@ namespace TestExport
   class Program
   {
     static string fileName = "DeletedFiles.csv";
+
     static void Main(string[] args)
     {
-      FileSystemWatcher watcher = new FileSystemWatcher(Properties.Settings.Default.Folder);
+        FileSystemWatcher watcher = new FileSystemWatcher(Properties.Settings.Default.Folder);
 
-      watcher.EnableRaisingEvents = true;
-      watcher.Created += Watcher_Created;
+        watcher.EnableRaisingEvents = true;
+        watcher.Created += LogEvents;
+        watcher.Changed += LogEvents;
+        watcher.Deleted += LogEvents;
+        watcher.Renamed += LogEvents;
 
-      File.AppendAllText(fileName, "DeletedTime;Filename;SampleNumber\n");
+        File.AppendAllText(fileName, "DeletedTime;Filename;SampleNumber;NumberOfColumns\n");
 
-      Console.Read();
+        Console.Read();
     }
 
     private static void Watcher_Created(object sender, FileSystemEventArgs e)
@@ -27,16 +31,24 @@ namespace TestExport
       string sampleNumber = "NoNumber";
       Thread.Sleep(1000);
       string[] lines = File.ReadAllLines(e.FullPath);
+      int noOfCols = 0;
 
       if (lines.Length>1)
       {
-        string resLine = lines[1];
-        sampleNumber = resLine.Split(',')[2];
+        string headerLine = lines[0];
+        var resultLine = lines[1];
+        sampleNumber = resultLine.Split(',')[4];
+        noOfCols = headerLine.Split(',').Length;
       }
 
-      string line = $"{DateTime.Now};{e.Name};{sampleNumber}\n";
+      string line = $"{DateTime.Now};{e.Name};{sampleNumber};{noOfCols}\n";
       File.Delete(e.FullPath);
       File.AppendAllText(fileName, line);
+    }
+
+    private static void LogEvents(object sender, FileSystemEventArgs e)
+    {
+        File.AppendAllText("Event.log",  $"{DateTime.Now.ToString("O")} {e.ChangeType} {e.Name}\n");
     }
   }
 }
