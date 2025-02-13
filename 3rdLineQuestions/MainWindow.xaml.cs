f864be64f9f0182563a9c9b5eb0661a10129ce30
Lars Busk <lab@foss.dk>
2025-02-13 12:23:45 +0100

    Now it works

--- 3rdLineQuestions/MainWindow.xaml.cs
+++ 3rdLineQuestions/MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ThirdLineQs;
 
 namespace _3rdLineQuestions
 {
@@ -16,19 +17,45 @@ namespace _3rdLineQuestions
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DataBaseHelper helper;
         public MainWindow()
         {
             InitializeComponent();
+            helper = new DataBaseHelper();
         }
 
         private void AddShortQuestion(object sender, RoutedEventArgs e)
         {
-
+            var kind = (string)((Button)sender).Content;
+            AddNewQuestion(GetKind(kind), 1);
         }
 
         private void AddLongQuestion(object sender, RoutedEventArgs e)
         {
+            var kind = (string)((Button)sender).Content;
+            AddNewQuestion(GetKind(kind), 2);
+        }
 
+        private void AddNewQuestion(short kind, short duration)
+        {
+            helper.AddQuestion(kind, duration);
+        }
+
+        private short GetKind(string kind)
+        {
+            switch (kind)
+            {
+                case "Software":
+                    return 1;
+                case "Hardware":
+                    return 2;
+                case "General":
+                    return 3;
+                case "Support":
+                    return 4;
+                default:
+                    return 5;
+            }
         }
     }
 }
