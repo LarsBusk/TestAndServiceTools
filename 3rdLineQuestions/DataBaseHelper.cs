aef60986e450ddbe953632d817c118eaeaf590e3
Lars Busk <lab@foss.dk>
2025-02-13 12:14:04 +0100

    Start of project

---
+++ 3rdLineQuestions/DataBaseHelper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+using ThirdLineQs.Model;
+
+namespace ThirdLineQs
+{
+    public class DataBaseHelper
+    {
+        private readonly ThirdLineQuestions dataContext;
+
+        public DataBaseHelper()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("config.json");
+            var config = builder.Build();
+            dataContext = new ThirdLineQuestions(config);
+        }
+
+        public void AddQuestion(short kind, short duration)
+        {
+            dataContext.Add(new Questions
+            {
+                Kind = kind,
+                Duration = duration,
+                QuestionTime = DateTime.Now
+            });
+            dataContext.SaveChanges();
+        }
+    }
+}
---
+++ 3rdLineQuestions/Model/ThirdLineQuestions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace ThirdLineQs.Model
+{
+    internal class ThirdLineQuestions : DbContext
+    {
+        public virtual DbSet<Enums> Enums { get; set; }
+        public virtual DbSet<Questions> Questions { get; set; }
+
+        protected readonly IConfiguration Configuration;
+
+        public ThirdLineQuestions(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseSqlServer(Configuration.GetConnectionString("ThirdLineQs"));
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Enums>();
+            modelBuilder.Entity<Questions>();
+        }
+    }
+}
